bebc01f3e0e781e474d5d53f6ca63337bbae475d
k8pai <thek8pai@gmail.com>
2023-06-23 23:25:13 +0530

    initial commit

---
+++ lib/data.ts
@@ -0,0 +1,34 @@
+const data = [
+	{
+		title: 'Post 1',
+		description: 'Description for Post 1',
+		tags: ['JavaScript', 'TypeScript', 'HTML'],
+		docUrl: 'https://k8pai-dev.vercel.app',
+		website: 'https://k8pai-dev.vercel.app',
+	},
+	{
+		title: 'Post 2',
+		description: 'Description for Post 2',
+		tags: ['Python', 'Django', 'Flask'],
+		postedBy: '_k8pai',
+		docUrl: 'https://k8pai-dev.vercel.app',
+		website: 'https://k8pai-dev.vercel.app',
+	},
+	{
+		title: 'Post 3',
+		description: 'Description for Post 3',
+		tags: ['Java', 'Spring', 'Hibernate'],
+		docUrl: 'https://k8pai-dev.vercel.app',
+		website: 'https://k8pai-dev.vercel.app',
+	},
+	{
+		title: 'Post 4',
+		description: 'Description for Post 4',
+		tags: ['C++', 'OpenGL', 'Qt'],
+		docUrl: 'https://k8pai-dev.vercel.app',
+		website: 'https://k8pai-dev.vercel.app',
+	},
+	// Add more objects as needed
+];
+
+export default data;
