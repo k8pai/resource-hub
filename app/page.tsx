acb0461cc8a162a07562af742c20c77e6c9e62b6
k8pai <thek8pai@gmail.com>
2023-06-24 23:56:58 +0530

    updated home page structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,60 @@
 import PostCard from '@/components/PostCard';
-import data from '@/lib/data';
+import { findAllResource } from '@/lib/prisma/resources';
+import { findAllTags } from '@/lib/prisma/tags';
+// import data from '@/lib/data';
+import { createUser, findAllUsers } from '@/lib/prisma/users';
+import { Resource, Tag, User, UserPayload } from '@prisma/client';
 import Image from 'next/image';
+import { FiSearch } from 'react-icons/fi';
 
-export default function Home() {
+export default async function Home() {
+	let posts: Resource[] | any = [],
+		tags: Tag[] | undefined = [];
+	try {
+		const { data: res, error: err } = await findAllResource();
+		posts = res;
+		const { data, error } = await findAllTags();
+		if (error) throw new Error('Error in tags/page.tsx');
+		tags = data;
+	} catch (error) {
+		console.log('catched in /app/page.tsx', error);
+	}
+
 	return (
 		<main className="h-full space-y-6 bg-[#141414] text-[#ECEFF1] max-w-2xl mx-auto">
-			{data?.map((el, elXid) => (
-				<PostCard key={elXid} postData={el} />
+			<section>
+				<div className={`relative rounded-lg overflow-hidden`}>
+					<input
+						name="tagName"
+						placeholder="tags..."
+						autoComplete="off"
+						className="pl-4 pr-10 py-3 rounded-lg backdrop-blur-sm bg-[#222222] focus:outline-none w-full tracking-wider font-semibold"
+					/>
+					<button className={`absolute right-1 inset-y-0 p-2`}>
+						<FiSearch />
+					</button>
+				</div>
+			</section>
+			<section>
+				<div className="flex flex-wrap items-center">
+					{tags?.map((el: Tag) => (
+						<div
+							key={el.id}
+							className="rounded-md w-fit bg-[#202020] tracking-wider font-semibold flex items-center space-x-px text-white text-xs px-2 py-1 m-1"
+						>
+							<span>#</span>
+							<span>{el?.name}</span>
+						</div>
+					))}
+				</div>
+			</section>
+			{posts?.map((el: any, elXid: number) => (
+				<PostCard
+					key={elXid}
+					postData={el}
+					createdBy={el.createdBy}
+					tags={el.tags}
+				/>
 			))}
 		</main>
 	);
