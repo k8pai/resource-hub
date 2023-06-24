927f02194abf63e318ca631cc6afa86f8a04f71b
k8pai <thek8pai@gmail.com>
2023-06-24 23:56:34 +0530

    added post route for creating post

---
+++ app/post/page.tsx
@@ -0,0 +1,12 @@
+import ResourceForm from '@/components/ResourceForm';
+import React from 'react';
+
+const Page = () => {
+	return (
+		<div>
+			<ResourceForm />
+		</div>
+	);
+};
+
+export default Page;
